ed756699bd86436f5e93e8948cf2a79d83412926
paramedics <unconfigured@null.spigotmc.org>
2022-12-03 10:22:58 +0500

    Uploaded build files

--- src/scripts/event-handler.ts
+++
@@ -1,9 +0,0 @@
-import { toggleAnswer } from './accordion'
-import { openMenu, closeMenu } from './navigation'
-
-document.querySelector('.open-menu-button')?.addEventListener('click', openMenu)
-document.querySelector('.close-menu-button')?.addEventListener('click', closeMenu)
-
-document.querySelectorAll('.question-text').forEach(questionText => {
-    questionText.addEventListener('click', event => toggleAnswer(event))
-})
--- src/scripts/navigation.ts
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector('.navigation-menu')
-
-export function openMenu() {
-    menu?.classList.add('navigation-menu-opened')
-}
-
-export function closeMenu() {
-    menu?.classList.remove('navigation-menu-opened')
-}
